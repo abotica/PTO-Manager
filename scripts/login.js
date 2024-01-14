3d3cb4862d27789bfc5a4a6e4170644260ad79cd
abotica <andrija.botica1613@icloud.com>
2024-01-14 23:10:34 +0100

    First commit, added starting files for project including some folders for structure of website.

---
+++ scripts/login.js
@@ -0,0 +1,11 @@
+
+const loginButton = document.getElementById("login-button")
+const enteredEmail = document.getElementById("email").value
+const enteredPassword = document.getElementById("password")
+
+loginButton.addEventListener("click", login())
+
+
+function login() {
+    console.log("hehe")
+}
